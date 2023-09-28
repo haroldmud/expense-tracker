324450f0f341c3b9177eb8210ece5c222d4bf021
haroldmud <haroldmudosa@email.com>
2023-09-28 17:32:17 +0200

    feat: implement the schema logic in auth service file

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,18 +5,13 @@ import { AppService } from './app.service';
 import { BudgetModule } from './budget/budget.module';
 import { ExpensesModule } from './expenses/expenses.module';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     BudgetModule,
     ExpensesModule,
-    MongooseModule.forRoot(
-      'mongodb+srv://budget:DUTt0i2obN6cz2X9@xo.6cb5vsr.mongodb.net/',
-    ),
+    MongooseModule.forRoot(process.env.DB_URI),
     AuthModule,
-    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,13 +5,22 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from './schemas/user.schema';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.registerAsync({
-      
-    })
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          secret: config.get<string>('JWT_SECRET'),
+          signOption: {
+            expiresIn: config.get<string | number>('JWT_EXPIRES'),
+          },
+        };
+      },
+    }),
     MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
   ],
   controllers: [AuthController],
