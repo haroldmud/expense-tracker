ac9c17049d3f5e5d63d3fe6ed795cbfbf9363b37
haroldmud <haroldmudosa@email.com>
2023-09-29 11:04:54 +0200

    fix: fix the way I was calling the injectables and add args to some of them

--- src/auth/dto/signUp.dto.ts
+++ src/auth/dto/signUp.dto.ts
@@ -1,15 +1,16 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
-export class Budget {
-  @isnotemp
-  item: string;
-  @Prop({ required: true })
-  budget: number;
-  @Prop({ required: true })
-  category: string;
-  @Prop({ required: true })
-  priority: string;
-  @Prop()
-  description?: string;
+export class SignUpDto {
+  @IsNotEmpty()
+  @IsString()
+  readonly name: string;
+
+  @IsNotEmpty()
+  @IsEmail({}, { message: 'please enter you email correcly' })
+  readonly email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(6)
+  readonly password: string;
 }
-
-export const BudgetSchema = SchemaFactory.createForClass(Budget);
