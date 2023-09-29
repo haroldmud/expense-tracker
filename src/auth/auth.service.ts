ce8a4a2d64755477dea235236fe582c8c71818be
haroldmud <haroldmudosa@email.com>
2023-09-29 10:42:47 +0200

    feat: implement the jwtService in the auth service for token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,16 +2,25 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './schemas/user.schema';
 import { Model } from 'mongoose';
-import * as   from 'bcrypt';
+import * as bcrypt  from 'bcrypt';
+import { JwtModule } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectModel(User.name)
     private userModel: Model<User>,
+    private jwtService: JwtModule,
   ) {}
   async signUp(signUpDto) {
     const { name, email, password } = signUpDto;
-    const crypting
+    const hashedPassword = await bcrypt.hash(password, 10);
+    // the 10 is kind of a default password, but tbh I don't know much hb it
+    const user = await this.userModel.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    return user;
   }
 }
