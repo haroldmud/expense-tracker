79cff78c9e1714b5c437722409cbe4d8fa54c48f
haroldmud <haroldmudosa@email.com>
2023-10-02 16:30:54 +0200

    feat: create the budget create DTO file

--- src/budget/dto/create-budget.dto.ts
+++ src/budget/dto/create-budget.dto.ts
@@ -1,1 +1,14 @@
-export class CreateBudgetDto {}
+import { IsNumber, IsString } from 'class-validator';
+
+export class CreateBudgetDto {
+  @IsString()
+  item: string;
+  @IsNumber()
+  budget: number;
+  @IsString()
+  category: string;
+  @IsString()
+  priority: string;
+  @IsString()
+  description?: string;
+}
