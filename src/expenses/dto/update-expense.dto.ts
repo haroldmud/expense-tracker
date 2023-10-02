fd734cf69cc6e133aa239f7d4a55a3d6173ae75c
haroldmud <haroldmudosa@email.com>
2023-10-02 16:28:55 +0200

    feat: refactor thee expense.controller.ts file

--- src/expenses/dto/update-expense.dto.ts
+++ src/expenses/dto/update-expense.dto.ts
@@ -1,10 +1,8 @@
-import { IsDate, IsNumber, IsString } from 'class-validator';
+import { IsNumber, IsString } from 'class-validator';
 
 export class UpdateExpenseDto {
   @IsString()
   spending: string;
   @IsNumber()
   price: number;
-  @IsDate()
-  time: Date;
 }
